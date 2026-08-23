ccccfdaa12693e8a1692053569e42bfe03881782
uosf90000-sketch <uosf90000@gmail.com>
2026-08-23 05:14:50 +0300

    style: load recipe POS styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import "./employee-access.css";
 import "./approved-ui.css";
 import "./statistics-dashboard.css";
 import "./smart-workflows.css";
+import "./recipe-pos.css";
 
 export const metadata: Metadata = {
   title: {
